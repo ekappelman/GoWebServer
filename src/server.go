b14b54515b8553708fa705efe1b6250100bf8b50
ekappelman <ekappelman@outlook.com>
2017-01-26 20:39:28 -0700

    Finished with it

--- src/server.go
+++ src/server.go
@@ -1,17 +1,69 @@
 package main
 
-import "io/ioutil"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
+//Page - webpage data type
 type Page struct {
 	Title string
 	Body  []byte
 }
 
 func (p *Page) save() error {
-	filename := p.Title + ".txt"
+	filename := "src/views/" + p.Title + ".txt"
 	return ioutil.WriteFile(filename, p.Body, 0600)
 }
 
+func loadPage(title string) (*Page, error) {
+	filename := "src/views/" + title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>"+
+		"<h3>Edit Page</h3>"+
+		"<form action=\"/edit/%s\" method=\"POST\">"+
+		"<input type=\"submit\" value=\"EDIT\">"+
+		"</form>", p.Title, p.Body, p.Title)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	fmt.Fprintf(w, "<h1>Editing %s</h1>"+
+		"<form action=\"/save/%s\" method=\"POST\">"+
+		"<textarea name=\"body\">%s</textarea><br>"+
+		"<input type=\"submit\" value=\"Save\">"+
+		"</form>",
+		p.Title, p.Title, p.Body)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	p.save()
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
 func main() {
-	//this
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
+	http.ListenAndServe(":8080", nil)
 }
